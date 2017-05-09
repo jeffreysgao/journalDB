8bf2c2a803153b02755cc0052b05ee882851fc0f
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-05-09 16:16:07 -0400

    Updated methods after testing insert

--- src/journalDB/people.java
+++ src/journalDB/people.java
@@ -7,8 +7,8 @@ public class people {
 	public static final int EDITOR = 1; 
 	public static final int REVIEWER = 2;
 	
-	public static int regReviewer(String fname, String lname, String mname, String affil, String email, ArrayList<Integer> ricodes) {
-		int id = regPerson(fname, lname, mname, REVIEWER);
+	public static long regReviewer(String fname, String lname, String mname, String affil, String email, ArrayList<Integer> ricodes) {
+		long id = regPerson(fname, lname, mname, REVIEWER);
 		if (id > 0) {
 			String rQuery = "INSERT INTO Reviewer (Person_PERSON_ID, REV_EMAIL, REV_ISACTIVE";
 			if (affil != null && !affil.isEmpty()) 
@@ -17,7 +17,7 @@ public class people {
 			if (affil != null && !affil.isEmpty()) 
 				rQuery += String.format(", %1", affil);
 			rQuery += ");";
-			if (query.execute(rQuery)) {
+			if (query.insert(rQuery) > 0) {
 				for (int ricode : ricodes) {
 					// query to insert ricodes
 				}
@@ -29,54 +29,47 @@ public class people {
 		return -1;
 	}
 	
-	public static int regAuthor(String fname, String lname, String mname, String email, String address, String affil) {
-		int id = regPerson(fname, lname, mname, AUTHOR);
+	public static long regAuthor(String fname, String lname, String mname, String email, String address, String affil) {
+		long id = regPerson(fname, lname, mname, AUTHOR);
 		if (id > 0) {
 			String aQuery = "INSERT INTO Author (Person_PERSON_ID, AUTH_ADDR, AUTH_EMAIL";
 			if (affil != null && !affil.isEmpty())
 				aQuery += ", AUTH_AFFIL";
-			aQuery += String.format(") VALUES (%1, %2, %3", id, address, email);
+			aQuery += String.format(") VALUES (\"%1$s\", \"%2$s\", \"%3$s\"", id, address, email);
 			if (affil != null && !affil.isEmpty()) 
-				aQuery += String.format(", %1", affil);
+				aQuery += String.format(", \"%1$s\"", affil);
 			aQuery += ");";
-			if (query.execute(aQuery))
+			if (query.insert(aQuery) > 0)
 				return id;
 		} 
 		return -1;
 	}
 	
-	public static int regEditor(String fname, String lname, String mname) {
+	public static long regEditor(String fname, String lname, String mname) {
 		return regPerson(fname, lname, mname, EDITOR);
 	}
 	
-	private static int regPerson(String fname, String lname, String mname, int role) {
-		String cQuery = "SELECT COUNT(*) FROM Person;";
-		int count = query.getInt(cQuery);
-		
-		if (count == -1)
-			return -1;
-		
+	private static long regPerson(String fname, String lname, String mname, int role) {
 		String pQuery = "INSERT INTO Person (PERSON_LNAME, PERSON_FNAME";
 		if (mname != null && !mname.isEmpty())
 			pQuery += ", PERSON_MNAME";
-		pQuery += String.format(", PERSON_ROLE) VALUES (%1, %2", lname, fname);
+		pQuery += String.format(", PERSON_ROLE) VALUES (\"%1$s\", \"%2$s\"", lname, fname);
 		if (mname != null && !mname.isEmpty())
-			pQuery += String.format(", %1", mname);
+			pQuery += String.format(", \"%1$s\"", mname);
 		switch(role) {
 			case AUTHOR: 
-				pQuery += ", author);";
+				pQuery += ", \"author\");";
 				break;
 			case EDITOR: 
-				pQuery += ", editor";
+				pQuery += ", \"editor\"";
 				break;
 			case REVIEWER: 
-				pQuery += ", reviewer";
+				pQuery += ", \"reviewer\"";
 				break;
 			default: 
 				return -1;
 		} 
 		
-		query.execute(pQuery);
-		return count++;
+		return query.insert(pQuery);
 	}
 }
--- src/journalDB/query.java
+++ src/journalDB/query.java
@@ -1,5 +1,6 @@
 package journalDB;
 
+import java.math.BigInteger;
 import java.sql.*;
 
 public class query {
@@ -7,9 +8,11 @@ public class query {
 	public static final String SERVER   = "jdbc:mysql://sunapee.cs.dartmouth.edu/";
     public static final String USERNAME = "gbsnlspl";
     public static final String PASSWORD = "dbTimespring2017!";
-    public static final String DATABASE = "cs61";
-//    public static final String QUERY    = "SELECT * FROM manuscript;";
+    public static final String DATABASE = "gbsnlspl_db";
 	
+    /*
+     * Used for queries that get tables, true indicates success
+     */
     public static boolean execute(String q) {
     	Connection con = null;
     	Statement stmt = null;
@@ -62,19 +65,76 @@ public class query {
     		con.close();
     		stmt.close();
     		res.close();
-    		System.out.print("\nConnection terminated.");
+    		System.out.println("\nConnection terminated.");
     		return true;
     	    } catch (Exception e) { /* ignore cleanup errors */ }
     	}
     	return false;
     }
     
-    public static int getInt(String q) {
+    /* 
+     * Used for queries that update tables, true indicates successfully changing 1+ rows
+     */
+    public static int insert(String q) {
+    	Connection con = null;
+    	Statement stmt = null;
+    	int res = 0;
+    	int numColumns = 0;
+    	int id = -1;
+
+    	// attempt to connect to db
+    	try {
+    	    // load mysql driver
+    	    Class.forName("com.mysql.jdbc.Driver").newInstance();
+    	    
+    	    // initialize connection
+    	    con = DriverManager.getConnection(SERVER+DATABASE, USERNAME, PASSWORD);
+    	    
+    	    System.out.println("Connection established.");
+
+    	    // initialize a query statement
+    	    stmt = con.createStatement();
+
+    	    // query db and save results
+    	    res = stmt.executeUpdate(q, Statement.RETURN_GENERATED_KEYS);
+    	    
+    	    ResultSet rs = stmt.getGeneratedKeys();
+    	    if (rs.next())
+    	    	id = rs.getInt(1);
+    	    rs.close();
+
+    	    System.out.format("Query executed: '%s'\n\nResults:\n", q);
+    	    
+    	} catch (SQLException e ) {          // catch SQL errors
+    	    System.err.format("SQL Error: %s", e.getMessage());
+    	    return -1;
+    	} catch (Exception e) {              // anything else
+    	    e.printStackTrace();
+    	    return -1;
+    	} finally {
+    	    // cleanup
+    	    try {
+    		con.close();
+    		stmt.close();
+    		System.out.println("\nConnection terminated.");
+    		
+    		if (res > 0)
+    			return id;
+    	    } catch (Exception e) { /* ignore cleanup errors */ }
+    	}
+    	return -1;
+    }
+    
+    
+    /*
+     * Used for queries that return a single column, single row table with a long value (count)
+     */
+    public static long getLong(String q) {
     	Connection con = null;
     	Statement stmt = null;
     	ResultSet res  = null;
     	int numColumns = 0;
-    	int count;
+    	long count = -1;
 
     	// attempt to connect to db
     	try {
@@ -100,8 +160,8 @@ public class query {
     	    if (numColumns != 1 || !res.next())
     	    	return -1;
     	   
-    	    count = (Integer)res.getObject(1);
-    	} catch (SQLException e ) {          // catch SQL errors
+    	    count = ((BigInteger)res.getObject(1)).longValue();
+       	} catch (SQLException e ) {          // catch SQL errors
     	    System.err.format("SQL Error: %s", e.getMessage());
     	    return -1;
     	} catch (Exception e) {              // anything else
@@ -113,7 +173,7 @@ public class query {
     		con.close();
     		stmt.close();
     		res.close();
-    		System.out.print("\nConnection terminated.");
+    		System.out.println("\nConnection terminated.");
     		return count;
     	    } catch (Exception e) { /* ignore cleanup errors */ }
     	}
