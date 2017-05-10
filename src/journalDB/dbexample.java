43941f45ba521042a827eb97c8783ea70b8d38c3
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-05-10 09:13:21 -0400

    dbexample push

--- src/journalDB/dbexample.java
+++ src/journalDB/dbexample.java
@@ -13,60 +13,10 @@ public class dbexample {
   public static final String USERNAME = "gbsnlspl";
   public static final String PASSWORD = "dbTimespring2017!";
   public static final String DATABASE = "gbsnlspl_db";
-  public static final String QUERY    = "SELECT * FROM Manuscript;";
+  public static final String QUERY    = "SELECT * FROM manuscript;";
 
   public static void main(String[] args) {
-	Connection con = null;
-	Statement stmt = null;
-	ResultSet res  = null;
-	int numColumns = 0;
-
-	// attempt to connect to db
-	try {
-	    // load mysql driver
-	    Class.forName("com.mysql.jdbc.Driver").newInstance();
-	    
-	    // initialize connection
-	    con = DriverManager.getConnection(SERVER+DATABASE, USERNAME, PASSWORD);
-	    
-	    System.out.println("Connection established.");
-
-	    // initialize a query statement
-	    stmt = con.createStatement();
-
-	    // query db and save results
-	    res = stmt.executeQuery(QUERY);
-
-	    System.out.format("Query executed: '%s'\n\nResults:\n", QUERY);
-	    
-	    // the result set contains metadata
-	    numColumns = res.getMetaData().getColumnCount();
-
-	    // print table header
-	    for(int i = 1; i <= numColumns; i++) {
-		System.out.format("%-12s", res.getMetaData().getColumnName(i));
-	    }
-	    System.out.println("\n--------------------------------------------");
-
-	    // iterate through results
-	    while(res.next()) {
-		for(int i = 1; i <= numColumns; i++) {
-		    System.out.format("%-12s", res.getObject(i));
-		}
-		System.out.println("");
-	    }
-	} catch (SQLException e ) {          // catch SQL errors
-	    System.err.format("SQL Error: %s", e.getMessage());
-	} catch (Exception e) {              // anything else
-	    e.printStackTrace();
-	} finally {
-	    // cleanup
-	    try {
-		con.close();
-		stmt.close();
-		res.close();
-		System.out.print("\nConnection terminated.");
-	    } catch (Exception e) { /* ignore cleanup errors */ }
-	}
+	long id = people.regAuthor("jeff", "gao", "shung", "jeffrey.s.gao", "13517 Moonflower Meadows Trail", "Dartmouth College");
+	System.out.println(Long.toString(id));
   }
 }
