94fc4ad608cb4b4c51f3f7df1bc4d339ec539867
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-05-11 14:57:32 -0400

    Updated dbexample for testing

--- src/journalDB/dbexample.java
+++ src/journalDB/dbexample.java
@@ -17,29 +17,39 @@ public class dbexample {
   public static final String QUERY    = "SELECT * FROM manuscript;";
 
   public static void main(String[] args) {
-//	long id = People.regAuthor("jeff", "gao", "shung", "jeffrey.s.gao", "13517 Moonflower Meadows Trail", "Dartmouth College");
-//	System.out.println(Long.toString(id));
+	long id = People.regAuthor("jeff", "gao", "shung", "jeffrey.s.gao1", "13517 Moonflower Meadows Trail", "Dartmouth College");
+	System.out.println(Long.toString(id));
+	   
+	  ArrayList<ArrayList<String>> results = Query.execute("SELECT * FROM Person;");
+	  for (ArrayList<String> row : results) {
+		  for (String value : row) {
+			  System.out.print(value + "\t");
+		  }
+		  System.out.println();
+	  }
 	  
-	  
-//	  ArrayList<ArrayList<String>> results = Query.execute("SELECT * FROM Person;");
-//	  for (ArrayList<String> row : results) {
-//		  for (String value : row) {
-//			  System.out.print(value + "\t");
-//		  }
-//		  System.out.println();
-//	  }
-	  
-//	  ArrayList<Integer> ricodes = new ArrayList<Integer>();
-//	  ricodes.add(1);
-//	  ricodes.add(2);
-//	  ricodes.add(3);
-//	  long id = People.regReviewer("jeff", "gao", null, "Dartmouth College", "jeffrey.s.gao", ricodes);
-//	  System.out.println(Long.toString(id));
+	  ArrayList<Integer> ricodes = new ArrayList<Integer>();
+	  ricodes.add(1);
+	  ricodes.add(2);
+	  ricodes.add(3);
+	  long revId = People.regReviewer("jeff", "gao", null, "Dartmouth College", "jeffrey.s.gao2", ricodes);
+	  System.out.println(Long.toString(revId));
 	  
 	  Editor jameson = new Editor(50);
-	  if (jameson.assign(3, 38))
+	  if (jameson.assign(3, (int)revId))
 		  System.out.println("success");
 	  else
 		  System.out.println("fail");
+	  
+	  if (jameson.reject(1))
+		  System.out.println("success");
+	  else
+		  System.out.println("fail");
+	  
+	  if (jameson.accept(3))
+		  System.out.println("success");
+	  else
+		  System.out.println("fail");
+	  
   }
 }
