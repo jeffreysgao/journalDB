79c89db3970e38d2a15aba88ba442eff1400a6c9
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-05-12 13:52:07 -0400

    Implemented status commands and reviewer resignation, updated testing file

--- src/journalDB/Reviewer.java
+++ src/journalDB/Reviewer.java
@@ -9,12 +9,68 @@ public class Reviewer extends Person {
 	}
 	
 	public void status() {
-		System.out.println("Reviewer status");
+		int submitted = 0, underReview = 0, rejected = 0, accepted = 0, typesetting = 0, scheduled = 0, published = 0;
+		
+		String revQuery = String.format("SELECT COUNT(*) FROM Assignment JOIN Manuscript ON Manuscript_MAN_ID = MAN_ID"
+				+ " WHERE Reviewer_Person_PERSON_ID = %1$s AND MAN_STATUS = 2;", this.id);
+		if (getCount(revQuery) >= 0)
+			underReview = getCount(revQuery);
+		String rejQuery = String.format("SELECT COUNT(*) FROM Assignment JOIN Manuscript ON Manuscript_MAN_ID = MAN_ID"
+				+ " WHERE Reviewer_Person_PERSON_ID = %1$s AND MAN_STATUS = 1;", this.id);
+		if (getCount(rejQuery) >= 0)
+			rejected = getCount(rejQuery);
+		String accQuery = String.format("SELECT COUNT(*) FROM Assignment JOIN Manuscript ON Manuscript_MAN_ID = MAN_ID"
+				+ " WHERE Reviewer_Person_PERSON_ID = %1$s AND MAN_STATUS = 3 AND MAN_ID NOT IN (SELECT Manuscript_MAN_ID FROM Scheduling)"
+				+ " AND MAN_NUMPAGES IS NULL;", this.id);
+		if (getCount(accQuery) >= 0)
+			accepted = getCount(accQuery);
+		String typeQuery = String.format("SELECT COUNT(*) FROM Assignment JOIN Manuscript ON Manuscript_MAN_ID = MAN_ID"
+				+ " WHERE Reviewer_Person_PERSON_ID = %1$s AND MAN_STATUS = 3 AND MAN_ID NOT IN (SELECT Manuscript_MAN_ID FROM Scheduling)"
+				+ " AND MAN_NUMPAGES IS NOT NULL;", this.id);
+		if (getCount(typeQuery) >= 0)
+			typesetting = getCount(typeQuery);
+		String schedQuery = String.format("SELECT COUNT(*) FROM Assignment JOIN Manuscript ON Manuscript_MAN_ID = MAN_ID"
+				+ " WHERE Reviewer_Person_PERSON_ID = %1$s AND MAN_STATUS = 3 AND MAN_ID IN (SELECT Manuscript_MAN_ID FROM Scheduling);", this.id);
+		if (getCount(schedQuery) >= 0)
+			scheduled = getCount(schedQuery);
+		String pubQuery = String.format("SELECT COUNT(*) FROM Assignment JOIN Manuscript ON Manuscript_MAN_ID = MAN_ID"
+				+ " WHERE Reviewer_Person_PERSON_ID = %1$s AND MAN_STATUS = 4;", this.id);
+		if (getCount(pubQuery) >= 0)
+			published = getCount(pubQuery);
+		
+		String result = String.format("%1$s submitted, %2$s under review, %3$s rejected, %4$s accepted, %5$s typesetting, %6$s scheduled, %7$s published", 
+				submitted, underReview, rejected, accepted, typesetting, scheduled, published);
+		
+		//produces a report of all the reviewer's
+		//manuscripts currently in the system where
+		//they are the primary reviewer
+		System.out.println(result);
 	}
 	
-	public void resign(){
+	public void getStatus() {
+		String manQuery = String.format("SELECT * FROM Manuscript WHERE MAN_ID IN"
+				+ " (SELECT Manuscript_MAN_ID FROM Assignment WHERE Reviewer_Person_PERSON_ID = %1$s) ORDER BY MAN_STATUS, MAN_ID;" , this.id);
+		ArrayList<ArrayList<String>> results = Query.execute(manQuery);
+		if (results.size() < 2) {
+			System.out.println("Error getting assigned manuscripts");
+			return;
+		} else {
+			System.out.println("ASSIGNED MANUSCRIPTS: ");
+		}
+		
+		for (int i = 1; i < results.size(); i++) {
+			String row = "\t" + i + ". ";
+			for (int j = 0; j < results.get(0).size(); j++) {
+				row += String.format("%1$s: %2$s\t", results.get(0).get(j), results.get(i).get(j));
+			}
+			System.out.println(row);
+		}
+	}
+	
+	public boolean resignation(){
 		String query = "UPDATE `Reviewer` SET REV_ISACTIVE = false WHERE Person_PERSON_ID = " + this.id;
-		Query.insert(query);
+		// value returned should be > 0 if at least one row is updated
+		return Query.insert(query) >= 0;
 	}
 	
 	public void review(Boolean isAccepted, ArrayList<String> input){
@@ -35,11 +91,33 @@ public class Reviewer extends Person {
 		}
 	}
 
-	private void review(Boolean isAccepted, int manId, int appr, int clar, int meth, int contr){	
+	public boolean review(Boolean isAccepted, int manId, int appr, int clar, int meth, int contr){	
+		// check if manuscript is assigned and unreviewed by you
+		String manQuery = String.format("SELECT COUNT(*) FROM Assignment WHERE Manuscript_MAN_ID = %1$s && Reviewer_Person_PERSON_ID = %2$s;"
+				, manId, this.id);
+		if (getCount(manQuery) <= 0) {
+			System.out.println("You are not assigned to this manuscript");
+			return false;
+		}
+		
+		String ratQuery = String.format("SELECT COUNT(*) FROM Rating WHERE Assignment_Reviewer_Person_PERSON_ID = %1$s AND "
+				+ "Assignment_Manuscript_MAN_ID = %2$s;", this.id, manId);
+		if (getCount(ratQuery) > 0) {
+			System.out.println("You have already reviewed this manuscript");
+			return false;
+		}
+			
 		int intAccepted = isAccepted ? 1 : 0; //1 is accepted, 0 not accepted
 		String query = "INSERT INTO Rating (Assignment_Reviewer_Person_PERSON_ID, Assignment_Manuscript_MAN_ID, RATING_APPR, RATING_CLAR, RATING_METH, RATING_CONTR, RATING_REC) " + 
 				"VALUES (" + this.id + ", " + manId + ", " + appr + ", " + clar + ", " + meth + ", " + contr + ", " + intAccepted +");";
-		System.out.println(query);
-		Query.insert(query);
+		return Query.insert(query) >= 0;
 	}	
+	
+	private int getCount(String query) {
+		ArrayList<ArrayList<String>> results = Query.execute(query);
+		if (results.size() != 2 || results.get(1).size() != 1) 
+			return -1;
+		
+		return Integer.parseInt(results.get(1).get(0));
+	}
 }
--- src/journalDB/dbexample.java
+++ src/journalDB/dbexample.java
@@ -24,16 +24,11 @@ public class dbexample {
 	  // test regAuthor
 	  System.out.println("TESTING REGAUTHOR:");
 	  long authId = People.regAuthor("jeff", "gao", "shung", "jeffrey.s.gao1", "13517 Moonflower Meadows Trail", "Dartmouth College");
-	  System.out.println();
-	   
-//	  // test method for parsing tables
-//	  ArrayList<ArrayList<String>> results = Query.execute("SELECT * FROM Person;");
-//	  for (ArrayList<String> row : results) {
-//		  for (String value : row) {
-//			  System.out.print(value + "\t");
-//		  }
-//		  System.out.println();
-//	  }
+	  if (authId > 0)
+		  System.out.println("success\n");
+	  else 
+		  System.out.println("fail\n");
+	  
 	  
 	  // test regReviewer
 	  System.out.println("TESTING REGREVIEWER:");
@@ -42,15 +37,20 @@ public class dbexample {
 	  ricodes.add(2);
 	  ricodes.add(3);
 	  long revId = People.regReviewer("jeff", "gao", null, "Dartmouth College", "jeffrey.s.gao2", ricodes);
-	  System.out.println();
+	  if (revId > 0)
+		  System.out.println("success\n");
+	  else 
+		  System.out.println("fail\n");
 	  
 	  /*
 	   * Author.java tests
 	   */
+	  System.out.println("INITIALIZING AUTHOR: ");
 	  Author dickens = new Author((int)authId);
+	  System.out.println();
 
 	  // submit
-	  System.out.println("TESTING SUBMIT");
+	  System.out.println("TESTING SUBMIT: ");
 	  long manId = dickens.submit("A Tale of Two Cities", "UCL", 23, null, null);
 	  if (manId > 0)
 		  System.out.println("success\n");
@@ -60,11 +60,23 @@ public class dbexample {
 	  // status
 	  System.out.println("TESTING STATUS:");
 	  dickens.status();
+	  System.out.println();
 	  
 	  /*
-	   * Editor.java tests
+	   * Editor.java and Reviewer.java tests
 	   */
+	  System.out.println("INTIALIZING EDITOR: ");
 	  Editor jameson = new Editor(50);
+	  System.out.println();
+	  
+	  // get status
+	  System.out.println("TESTING EDITOR GETSTATUS: ");
+	  jameson.getStatus();
+	  System.out.println();
+	  
+	  System.out.println("INITIALIZING REVIEWER: ");
+	  Reviewer jgao = new Reviewer((int)revId);
+	  System.out.println();
 	  
 	  // assign 
 	  System.out.println("TESTING ASSIGN: ");
@@ -73,9 +85,23 @@ public class dbexample {
 	  else
 		  System.out.println("fail\n");
 	  
+	  // review 
+	  System.out.println("TESTING REVIEW: ");
+	  if (jgao.review(true, (int)manId, 9, 8, 7, 9))
+		  System.out.println("success\n");
+	  else
+		  System.out.println("fail\n");
+	  
+	  jgao.review(true, (int)manId, 9, 8, 7, 9);
+	  System.out.println();
+	  jgao.review(true, 1, 9, 8, 7, 9);
+	  System.out.println();
+	  
 	  // status
 	  System.out.println("TESTING STATUS:");
 	  dickens.status();
+	  jgao.status();
+	  System.out.println();
 	  
 	  // reject
 	  System.out.println("TESTING REJECT: ");
@@ -104,6 +130,8 @@ public class dbexample {
 	  // status
 	  System.out.println("TESTING STATUS:");
 	  dickens.status();
+	  jgao.status();
+	  System.out.println();
 	  
 	  // typeset
 	  System.out.println("TESTING TYPESET: ");
@@ -115,7 +143,9 @@ public class dbexample {
 	  // status
 	  System.out.println("TESTING STATUS:");
 	  dickens.status();
-	 
+	  jgao.status();
+	  System.out.println();
+
 	  // schedule
 	  System.out.println("TESTING SCHEDULE: ");
 	  if (jameson.schedule((int)manId, 20))
@@ -141,17 +171,38 @@ public class dbexample {
 	  // status
 	  System.out.println("TESTING STATUS:");
 	  dickens.status();
+	  jgao.status();
+	  System.out.println();
+
+	  // publish
+	  System.out.println("TESTING PUBLISH: ");
+	  if (jameson.publish(20))
+		  System.out.println("success\n");
+	  else 
+		  System.out.println("fail\n");
 	  
 	  // retract
 	  System.out.println("TESTING RETRACT: ");
 	  dickens.retract((int)manId);
+	  System.out.println();
 	  
-//	  // publish
-//	  System.out.println("TESTING PUBLISH: ");
-//	  if (jameson.publish(20))
-//		  System.out.println("success\n");
-//	  else 
-//		  System.out.println("fail\n");
-//	  
+	  // status
+	  System.out.println("TESTING STATUS:");
+	  dickens.status();
+	  jgao.status();
+	  System.out.println();
+
+	  // get status
+	  System.out.println("TESTING GET STATUS: ");
+	  Reviewer rev = new Reviewer(15);
+	  rev.getStatus();
+	  System.out.println();
+	  
+	  // resignation
+	  System.out.println("TESTING RESIGNATION: ");
+	  if (jgao.resignation())
+		  System.out.println("success");
+	  else 
+		  System.out.println("failure");
   }
 }
