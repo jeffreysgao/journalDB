253f39216d4f8615d9b1c688a457932668202f8a
Samuel Lee <sleeinsuk@gmail.com>
2017-05-12 11:04:38 -0400

    pre-pull commit

--- src/journalDB/dbexample.java
+++ src/journalDB/dbexample.java
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 
 public class dbexample {
   public static final String SERVER   = "jdbc:mysql://sunapee.cs.dartmouth.edu/";
-  public static final String USERNAME = "gbsnlspl";
-  public static final String PASSWORD = "dbTimespring2017!";
-  public static final String DATABASE = "gbsnlspl_db";
+  public static final String USERNAME = "salee";
+  public static final String PASSWORD = "sleeslee";
+  public static final String DATABASE = "salee_db";
   public static final String QUERY    = "SELECT * FROM manuscript;";
 
   public static void main(String[] args) {
